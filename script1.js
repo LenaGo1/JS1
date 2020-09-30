ef9353bc1c1856da1eba6e8509aef24f3527bee5
LenaGo1 <lena.goyeva@gmail.com>
2020-09-30 15:19:36 +0300

    делает скрипты для домашки1

---
+++ script1.js
@@ -0,0 +1,10 @@
+
+const name = "«Генератор защитного поля»"; // Write code on this line
+let price = 1000; // Write code on this line
+firstMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; // Write code on this line
+
+price = 2000;
+secondMessage = `Выбран ${name}, цена за штуку ${price} кредитов`; // Write code on this line
+// 'Выбран «Генератор защитного поля», цена за штуку 1000 кредитов'
+console.log(firstMessage);
+console.log(secondMessage);
